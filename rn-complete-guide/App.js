c2dde166e2247b14e0e3c9d55391764991062391
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-11 17:57:19 +0300

    21. Getting Started with Styles

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ padding: 50 }}>
       <View>
-        ...
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 10
+          }}
+        />
+        <Button title="Add" />
       </View>
 
-      <View>
-        
-      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
