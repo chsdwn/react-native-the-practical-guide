9a5e40b348192943b47172e4eb4e02eae6a14232
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-17 17:25:13 +0300

    68. Finishing the Game Logic

--- guess-a-number/screens/GameOverScreen.js
+++ guess-a-number/screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>Gameover</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
 };
