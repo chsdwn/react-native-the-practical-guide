c0dff68a9ec41fe3144c108cacc3b8d9b09bf85e
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-24 19:35:20 +0300

    168. Outputting Cart Items

---
+++ the-shop/components/shop/CartItem.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export const CartItem = ({ quantity, title, amount, onRemove }) => {
+  return (
+    <View style={styles.cartItem}>
+      <View style={styles.itemData}>
+        <Text style={styles.quantity}>{quantity} </Text>
+        <Text style={styles.defaultText}>{title}</Text>
+      </View>
+      <View style={styles.itemData}>
+        <Text style={styles.defaultText}>${amount.toFixed(2)}</Text>
+        <TouchableOpacity style={styles.deleteButton} onPress={onRemove}>
+          <Ionicons
+            name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
+            size={23}
+            color={"#f00"}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  cartItem: {
+    padding: 10,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20
+  },
+  itemData: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  quantity: {
+    fontFamily: "open-sans",
+    color: "#888",
+    fontSize: 16
+  },
+  defaultText: {
+    fontFamily: "open-sans-bold",
+    fontSize: 16
+  },
+  deleteButton: {
+    marginLeft: 20
+  }
+});
--- the-shop/screens/shop/CartScreen.js
+++ the-shop/screens/shop/CartScreen.js
@@ -2,10 +2,26 @@ import React from "react";
 import { Button, FlatList, StyleSheet, Text, View } from "react-native";
 import { useSelector } from "react-redux";
 
+import { CartItem } from "../../components/shop/CartItem";
+
 import Colors from "../../constants/Colors";
 
 export const CartScreen = props => {
   const cart = useSelector(state => state.cart);
+  const cartItems = useSelector(state => {
+    const transformedCartItems = [];
+    for (const key in state.cart.items) {
+      transformedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum
+      });
+    }
+    console.log(transformedCartItems);
+    return transformedCartItems;
+  });
 
   return (
     <View style={styles.screen}>
@@ -20,6 +36,18 @@ export const CartScreen = props => {
           disabled={cart.totalAmount === 0}
         />
       </View>
+      <FlatList
+        data={cartItems}
+        keyExtractor={item => item.productId}
+        renderItem={itemData => (
+          <CartItem
+            quantity={itemData.item.quantity}
+            title={itemData.item.productTitle}
+            amount={itemData.item.sum}
+            onRemove={() => {}}
+          />
+        )}
+      />
     </View>
   );
 };
