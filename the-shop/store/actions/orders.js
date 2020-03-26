3f48b885fe17cf510171e7ca68e79d5fa8d28732
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-26 16:40:45 +0300

    204. Storing Orders

--- the-shop/store/actions/orders.js
+++ the-shop/store/actions/orders.js
@@ -1,8 +1,38 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+  return async dispatch => {
+    const date = new Date().toISOString();
+
+    const response = await fetch(
+      `https://the-shop-dde6a.firebaseio.com/orders/${"u1"}.json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+
+    const responseData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: responseData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date
+      }
+    });
   };
 };
