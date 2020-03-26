a8df42e7ae66dcc32b6d71a58c682fd9cac00a1e
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-26 13:55:43 +0300

    198. Storing Products on a Server

--- the-shop/store/actions/products.js
+++ the-shop/store/actions/products.js
@@ -3,9 +3,30 @@ export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 export const createProduct = (title, description, imageURL, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: { title, description, imageURL, price }
+  return async dispatch => {
+    const response = await fetch(
+      "https://the-shop-dde6a.firebaseio.com/products.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, description, imageURL, price })
+      }
+    );
+
+    const responseData = await response.json();
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: responseData.name,
+        title,
+        description,
+        imageURL,
+        price
+      }
+    });
   };
 };
 
