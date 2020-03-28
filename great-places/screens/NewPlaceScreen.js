95e481503756f12da2c94a3c208d5d94f02bf845
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-28 16:32:26 +0300

    228. Getting Started with the Form

--- great-places/screens/NewPlaceScreen.js
+++ great-places/screens/NewPlaceScreen.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+import Colors from "../constants/Colors";
 
 export const NewPlaceScreen = (props) => {
+  const [title, setTitle] = useState("");
+
+  const titleChangeHandler = (text) => {
+    setTitle(text);
+  };
+
+  const savePlaceHandler = () => {};
+
   return (
-    <View>
-      <Text>New Place</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          value={title}
+          onChange={titleChangeHandler}
+        />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -13,4 +42,19 @@ NewPlaceScreen.navigationOptions = {
   headerTitle: "Add Place",
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
